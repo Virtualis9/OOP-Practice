f3a3af178eac1f4013a0b870f08b406acb2fb904
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-06-11 18:58:35 +0100

    finished constructor function

---
+++ classes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Person = function(firstName, birthYear){
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    
+
+
+
+}
+
+const chris = new Person('chris', 1991)
+console.log(chris)
